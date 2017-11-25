1944231edaa9d564e05f55fe7e40158b8d0c2f7f
Shintani Teppei <teppest1215@gmail.com>
2017-11-25 13:14:42 +0900

    fix

--- webapp/ruby/game.rb
+++ webapp/ruby/game.rb
@@ -2,7 +2,7 @@ require 'concurrent-edge'
 require 'faye/websocket'
 require 'json'
 require 'mysql2'
-require 'pry'
+# require 'pry'
 
 class Game
   module Jsonable
@@ -108,27 +108,6 @@ class Game
         conn.close
       end
 
-    end
-
-    set_global_mitems
-    p $m_items
-    def set_global_mitems
-      $m_items = conn.query('SELECT * FROM m_item').map do |raw_item|
-        [
-          raw_item['item_id'].to_s, 
-          MItem.new(
-            item_id: raw_item['item_id'],
-            power1: raw_item['power1'],
-            power2: raw_item['power2'],
-            power3: raw_item['power3'],
-            power4: raw_item['power4'],
-            price1: raw_item['price1'],
-            price2: raw_item['price2'],
-            price3: raw_item['price3'],
-            price4: raw_item['price4'],
-          )
-        ].to_h
-      end
     end
 
     def str2big(s)
